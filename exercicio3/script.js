1ce75dd455a9c54b75dc0937bb5e897db736f188
Nayara <nayaraazevwdo@gmail.com>
2022-08-10 14:46:37 -0300

    condicionais1-exercicio-final

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,13 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt("Escreva aqui sua nacionalidade").toLowerCase()
+
+if(nacionalidade === "brasileira"){
+    console.log("Você é",nacionalidade)
+}else if(nacionalidade === "argentina"){
+    console.log("Você é",nacionalidade)
+}else if(nacionalidade === "uruguaia"){
+    console.log("Você é",nacionalidade)
+}else if(nacionalidade === "chilena"){
+    console.log("Você é",nacionalidade)
+}else if(nacionalidade === "colombiana"){
+    console.log("Você é",nacionalidade)
+}else(console.log("nacionalidade não encontrada"))
